9333a70eee642be187a95f3c3fbea29a7877763f
jriccrussel <jriccrussel@gmail.com>
2022-10-11 09:16:16 +0800

    add app.use(express.static) apply middleware to all routes

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -18,12 +18,18 @@ const morgan = require('morgan')
 //     // console.log("%c 🍆 person_data", "color:#b03734", people);
 // })
 
+// app.use applies the middleware to all the routes
+// static assets 
+app.use(express.static('./methods-public'))
+
 app.get('/api/people', (req, res) => {
     const data = fs.readFileSync(person_data, 'utf8')
     const passed_data = JSON.parse(data)
     // console.log("%c 🥔 passed_data", "color:#b03734", passed_data);
     res.status(200).json({ success: true, data: passed_data })
 })
+
+// POST - applies 
 
 app.listen(5000, () => {
     console.log('listening on port 5000')
