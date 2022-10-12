7dc34fac5e3a4e6bb8cd79b86788ed83caabe15f
jriccrussel <jriccrussel@gmail.com>
2022-10-12 16:54:13 +0800

    add app.post method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -10,6 +10,7 @@ const path_data = require('./data.json')
 const person_data = './person.json'
 // thirda party middleware
 const morgan = require('morgan')
+const { prependOnceListener } = require('process')
 
 // app.get('./api/people', (req, res) => {
 //     // const data = fs.readFileSync(person_data, 'utf8')
@@ -23,8 +24,26 @@ const morgan = require('morgan')
 app.use(express.static('./methods-public'))
 
 // parse(to split and analyze the data) form data
+// to get the form data
 // perform post request in url and g handle nato sa atong server to get our data
 app.use(express.urlencoded({ extended: false }))
+
+// parse json
+// handling incoming json data | send to our json data
+app.use(express.json())
+
+// reading data from people.json and add data to people.json
+// even tho ang app.post ang '/api/people' same as app.get it doesnt mean na same ang function nya sa get it works diffrently sa app.get
+app.post('/api/people', (req, res) => {
+    const { name } = req.body
+    if(!name){
+        return res.status(400).json({ success: false, message: 'please provide name value' })    
+    }
+    // 201 -means if send a post request
+    // res.status(201).json('Success')
+    // person - key from fetchPeople() na ato ge map
+    res.status(201).json({success: true, person: name})
+})
 
 app.get('/api/people', (req, res) => {
     const data = fs.readFileSync(person_data, 'utf8')
@@ -33,7 +52,24 @@ app.get('/api/people', (req, res) => {
     res.status(200).json({ success: true, data: passed_data })
 })
 
-// POST - applies 
+// add
+app.post('/api/people/add', (req, res) => {
+    const { id, name } = req.body
+    if(!name){ 
+        return res.status(400).json({ success: false, message: 'please provide name value' })
+    } 
+    let read = fs.readFileSync(person_data, 'utf-8')
+    let send_data = JSON.parse(read)
+    // let map_data = send_data.map(data => {data.id, data.name})
+    send_data.push(req.body)
+    // map_data.push(req.body)
+    let stringify_data = JSON.stringify(send_data, null, 2)
+    // let stringify_data = JSON.stringify([...map_data], null, 4)
+    fs.writeFileSync(person_data, stringify_data, 'utf-8')
+    res.status(201).json({ success: true })
+})
+
+// POST - applies  
 app.post('/login', (req, res) => {
     const { name } = req.body
     if(name){
